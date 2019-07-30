75764b840bfa0feb178b44ea53698087db64a788
Tania <tania.juspin@gmail.com>
2019-07-30 11:25:47 +0200

    finition du script

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+/*const typeWriter = () => {
+	if(i < texteArray.length) {
+		paragraphe.insertAdjacentHTML('beforeend',texteArray[i]);
+		i++;
+		setTimeout(typeWriter,80);
+	}
+};
+
+typeWriter();*/
+
+	let i = 0;
+	const paragraphe = document.getElementById('container_writer');
+
+const typeWriter = () => {
+
+	const texte = document.getElementById("phrase").value;
+	const texteArray = texte.split("");
+
+
+	paragraphe.insertAdjacentHTML('beforeend',texteArray[i]);
+	i++;
+
+	if(i >= texteArray.length)
+	{
+		clearInterval(interval);
+		i =0;
+	}
+};
+
+
+document.getElementById("submit").addEventListener('click', function() {paragraphe.innerHTML = ""});
+document.getElementById("submit").addEventListener('click', function() {interval = setInterval(typeWriter,80)});
